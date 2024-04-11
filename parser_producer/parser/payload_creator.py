4255dc7e0cd745c5d006da2d89a42836ae9d4438
Aleksey Kutanov <aleksey.kutanov@x5.ru>
2024-04-11 15:53:10 +0300

    ref

--- parser_producer/parser/payload_creator.py
+++ parser_producer/parser/payload_creator.py
@@ -1,5 +1,4 @@
 def create_payload(data: list[dict], **kwargs) -> list[dict]:
     n = kwargs.get("n")
     result = [{"title": block[0], "url": block[1]} for block in data][:n]
-    print(len(result))
     return result
