b9ff3d81ebd02650a3fda7e18d2001f6e0678680
Aleksey Kutanov <aleksey.kutanov@x5.ru>
2024-04-29 11:40:39 +0300

    refactoring

--- parser_producer/scraper/processing_soup_data.py
+++ parser_producer/scraper/processing_soup_data.py
@@ -1,11 +1,15 @@
 def bbc_news_parser(soup, **kwargs):
     text_and_links = soup.find_all("a", {"class": "media__link"})
+    print("FROM BBC: ", text_and_links, "\n\n\n")
     final_data = [[block.text.strip(), block.get("href")] for block in text_and_links]
+    print("FROM BBC: ", final_data, "\n\n\n")
     dict_content = {
         i[0]: i[1] if kwargs.get("url") in i[1] else f"{kwargs.get('url')}{i[1]}"
         for i in final_data
     }
+    print("FROM BBC: ", dict_content, "\n\n\n")
     final_data = [[k, v] for k, v in dict_content.items()]
+    print("FROM BBC: ", final_data, "\n\n\n")
     return final_data
 
 
