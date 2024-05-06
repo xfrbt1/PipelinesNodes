a6e515ee5522e89f89e7f5634c989bdaf09d860c
Aleksey Kutanov <aleksey.kutanov@x5.ru>
2024-05-06 12:44:04 +0300

    refactoring

--- api/run.py
+++ api/run.py
@@ -2,13 +2,14 @@ import uvicorn
 from starlette.applications import Starlette
 from starlette.routing import Route
 
-from api.api_handlers.news import search_titles
+from api.api_handlers.news import search_titles, all_titles
 
 if __name__ == "__main__":
     uvicorn.run(
         Starlette(
             routes=[
                 Route("/search-titles", search_titles),
+                Route("/titles", all_titles),
             ]
         ),
         host="0.0.0.0",
